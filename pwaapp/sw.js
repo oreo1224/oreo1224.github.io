358bd6056b858af0a0462c13628d6d83a1cc4131
oreo1224 <82581822+oreo1224@users.noreply.github.com>
2024-01-15 19:41:19 +0900

    Add files via upload

---
+++ pwaapp/sw.js
@@ -0,0 +1,28 @@
+// Cache name
+const CACHE_NAME = "janbookoff";
+// Cache targets
+const urlsToCache = [
+  "./",
+  "index.html",
+  "main.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
